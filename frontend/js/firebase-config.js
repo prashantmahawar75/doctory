57ca62524777f4d19ef52de1322c5c0104826bd2
prashantmahawar75 <146577072+prashantmahawar75@users.noreply.github.com>
2025-08-07 19:51:17 +0530

    Update firebase-config.js

--- frontend/js/firebase-config.js
+++ frontend/js/firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMKE1ZDj8ZE9fBJm7Y7NhXzj0VWLugl74",
-  authDomain: "doctor-appointment-app-63cd2.firebaseapp.com",
-  projectId: "doctor-appointment-app-63cd2",
-  storageBucket: "doctor-appointment-app-63cd2.firebasestorage.app",
-  messagingSenderId: "410094677145",
-  appId: "1:410094677145:web:edd6397e983bc1ceffdc08",
-  measurementId: "G-2VP21YN57E"
+  apiKey: "  ",
+  authDomain: "",
+  projectId: " ",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
